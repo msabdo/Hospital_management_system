985d866b4a508ce12eabb9e13c3ad7b671f6bfa4
Mohamed_AS <38574606+msabdo@users.noreply.github.com>
2020-09-03 14:33:43 +0200

    finishing routes

---
+++ app.py
@@ -0,0 +1,268 @@
+# from flask_cors import CORS
+from flask import Flask, jsonify, request, json
+from models import setup_app, db_drop_and_create_all, Patient, Doctor
+from sqlalchemy.exc import IntegrityError
+
+app = Flask(__name__)
+setup_app(app)
+
+
+# CORS(app)
+# db_drop_and_create_all()
+
+# ------------------------ patients routes ---------------#
+@app.route('/patients')
+def get_all_patients():
+    patients_list = []
+    try:
+        patients = Patient.query.all()
+        if not patients:
+            return jsonify({
+                "success": True,
+                "description": "patients not found"
+            }), 200
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    for patient in patients:
+        # doctor = Doctor.query.get(patient.doctor_id).short_format()
+        patients_list.append(patient.format())
+
+    return jsonify({
+        "success": True,
+        "patients": patients_list
+    }), 200
+
+
+@app.route('/patients/<int:patient_id>')
+def get_patient(patient_id):
+
+    try:
+        patient = Patient.query.get(patient_id)
+        if not patient:
+            return jsonify({
+                "success": True,
+                "description": "patient not found"
+            }), 404
+
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "patient": patient.format()
+    }), 200
+
+
+@app.route('/patients', methods=['POST'])
+def create_patient():
+    patient_data = json.loads(request.data)
+
+    doctor = Doctor.query.get(patient_data['doctor_id'])
+    if doctor == None:
+        return jsonify({
+            "success": False,
+            "description": "doctor id not found"
+        }), 404
+
+    patient = Patient(
+        name=patient_data["name"],
+        age=patient_data["age"],
+        address=patient_data["address"],
+        email=patient_data["email"],
+        examine_report=patient_data["examine_report"],
+    )
+    patient.doctor = doctor
+    try:
+        patient.insert()
+    except IntegrityError as err:
+        print(err)
+        return jsonify({
+            "success": False,
+            "description": "there is patient with the same email"
+        }), 200
+    except Exception as err:
+        print(err)
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "patient": Patient.query.get(patient.id).format()
+    }), 200
+
+
+@app.route('/patients/<int:patient_id>', methods=['PUT'])
+def update_patient(patient_id):
+    patient_data = json.loads(request.data)
+    doctor = Doctor.query.get(patient_data['doctor_id'])
+
+    if not doctor:
+        return jsonify({
+            "success": False,
+            "description": "doctor id not found"
+        }), 404
+    try:
+        patient = Patient.query.get(patient_id)
+        if not patient:
+            return jsonify({
+                "success": True,
+                "description": "patient not found"
+            }), 404
+        else:
+            patient.doctor = doctor
+            patient.update(patient_data)
+    except IntegrityError:
+        return jsonify({
+            "success": False,
+            "description": "there is patient with the same email"
+        }), 200
+
+    except Exception as err:
+        print("doctor update: ", err)
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "patient": patient.format()
+    }), 200
+
+
+@app.route('/patients/<int:patient_id>', methods=['DELETE'])
+def delete_patient(patient_id):
+    try:
+        patient = Patient.query.get(patient_id)
+        if not patient:
+            return jsonify({
+                "success": True,
+                "description": "patient already not found"
+            }), 200
+        else:
+            patient.delete()
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "patient_id ": patient_id
+    }), 200
+
+
+# ------------------------------------------------------------------------------#
+# ------------------------------------ doctors routes ---------------------------#
+@app.route('/doctors')
+def get_all_doctors():
+    doctors_list = []
+    try:
+        doctors = Doctor.query.all()
+        if not doctors:
+            return jsonify({
+                "success": True,
+                "description": "doctors not found"
+            }), 404
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    for doctor in doctors:
+        doctors_list.append(doctor.format())
+
+    return jsonify({
+        "success": True,
+        "doctors": doctors_list
+    }), 200
+
+
+@app.route('/doctors/<int:doctor_id>')
+def get_doctor(doctor_id):
+    try:
+        doctor = Doctor.query.get(doctor_id)
+        if not doctor:
+            return jsonify({
+                "success": True,
+                "description": "doctor not found"
+            }), 200
+
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "doctor": doctor.format()
+    }), 200
+
+
+@app.route('/doctors', methods=['POST'])
+def create_doctor():
+    doctor_data = json.loads(request.data)
+    doctor = Doctor(
+        name=doctor_data["name"],
+        address=doctor_data["address"],
+        salary=doctor_data["salary"],
+        email=doctor_data["email"]
+    )
+    try:
+        doctor.insert()
+    except IntegrityError:
+        return jsonify({
+            "success": False,
+            "description": "there is doctor with the same email"
+        })
+    except Exception :
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "doctor": doctor.format()
+    }), 200
+
+
+@app.route('/doctors/<int:doctor_id>', methods=['PUT'])
+def update_doctors(doctor_id):
+    doctor_data = json.loads(request.data)
+    try:
+        doctor = Doctor.query.get(doctor_id)
+        if not doctor:
+            return jsonify({
+                "success": True,
+                "description": "doctor not found"
+            }), 404
+        else:
+            doctor.update(doctor_data)
+    except Exception:
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "patient": doctor.format()
+    }), 200
+
+
+@app.route('/doctors/<int:doctor_id>', methods=['DELETE'])
+def delete_doctor(doctor_id):
+    try:
+        doctor = Doctor.query.get(doctor_id)
+        if not doctor:
+            return jsonify({
+                "success": True,
+                "description": "doctor already not found"
+            }), 200
+        else:
+            doctor.delete()
+    except IntegrityError as err:
+        print("delete doctor: ", err)
+        return jsonify({
+            "success": False,
+            "description": "cannot delete doctor 'he has patients', please reassign the patients "
+        }), 500
+
+    except Exception as err:
+        print("delete doctor: ", err)
+        return jsonify({"success": False}), 500
+
+    return jsonify({
+        "success": True,
+        "doctor_id ": doctor_id
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,109 @@
+from flask_sqlalchemy import SQLAlchemy
+
+database_name = 'hospital_management'
+database_path = "postgres://{}:{}@{}/{}".format('postgres', 'root', 'localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+
+def setup_app(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+
+
+class Doctor(db.Model):
+    __tablename__ = 'doctor'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String(120))
+    salary = db.Column(db.Integer)
+    email = db.Column(db.String(100), unique=True)
+    patients = db.relationship('Patient', backref=db.backref('doctor'))
+
+    def __init__(self, name, address, salary, email):
+        self.name = name
+        self.address = address
+        self.salary = salary
+        self.email = email
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        self.name = data["name"]
+        self.address = data["address"]
+        self.salary = data["salary"]
+        self.email = data["email"]
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def short_format(self):
+        return {
+            "name": self.name,
+            "email": self.email
+        }
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "address": self.address,
+            "salary": self.salary,
+            "email": self.email
+        }
+
+
+class Patient(db.Model):
+    __tablename__ = 'patient'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    address = db.Column(db.String(120))
+    email = db.Column(db.String(100),  unique=True)
+    examine_report = db.Column(db.String)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'), nullable=False)
+
+    def __init__(self, name, age, address, email, examine_report):
+        self.name = name
+        self.age = age
+        self.address = address
+        self.email = email
+        self.examine_report = examine_report
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        self.name = data["name"]
+        self.age = data["age"]
+        self.address = data["address"]
+        self.email = data["email"]
+        self.examine_report = data["examine_report"]
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "address": self.address,
+            "email": self.email,
+            "examine_report": self.examine_report,
+            "doctor": Doctor.query.get(self.doctor_id).short_format()
+        }
